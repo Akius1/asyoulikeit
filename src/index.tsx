6a59f4074c79728242064a308df7ba49fe2e2d17
Andrew Urom <andrewurom@gmail.com>
2023-02-10 13:47:48 +0100

    added pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {createRoot} from "react-dom/client";
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = createRoot(document.getElementById("root"));
 
@@ -9,3 +10,8 @@ root.render(
         <App />
     </React.StrictMode>
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
